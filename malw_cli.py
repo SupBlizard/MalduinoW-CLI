768ac34cdef4daeff06314f39af4efaefd8a0a29
SupBlizard <58865670+SupBlizard@users.noreply.github.com>
2023-10-01 17:49:13 +0100

    Rewrite main and finish basic WSC class

--- malw_cli.py
+++ malw_cli.py
@@ -2,101 +2,41 @@ import websocket, time, argparse
 from threading import Thread, Lock
 
 
-
+SERVER_IP = "192.168.4.1"
 
 
 def main(args):
-    ws = WebSocketClient("192.168.4.1", args.trace)
-    ws.connect()
+    malw = WebSocketClient(SERVER_IP, args.debug)
+    malw.connect()
 
-    print(wait_for(ws.is_connected, True, delay=0.25, timeout=5))
-    print(execute_cmd(ws, "version"))
-
-    # while True:
-    #     if crt_pkt != None:
-    #         time.sleep(0.5)
-    #         continue
-
-    #     try:
-    #         # Get input
-    #         pkt = input("> ")
-    #     except EOFError:
-    #         ws.close()
-    #         exit(0)
+    # Await connection to be established
+    if not wait_for(malw.is_connected, True, delay=0.25, timeout=5):
+        malw.disconnect()
+        raise websocket.WebSocketTimeoutException("Failed to establish connection")
         
-    #     # Exit case
-    #     if pkt == "exit":
-    #         ws.close()
-    #         exit(0)
-
-    #     # Send packet
-    #     crt_pkt = pkt
-    #     send_pkt(pkt)
+    print(" Connection Established ".center(100, "="))
+    print("Firmware " + malw.execute("version"))
 
 
-
-
-
-class WebSocketClient:
-    def __init__(self, server:str, trace:bool):
-        self.server = server
-        self.ws = None
-        self.debug = trace
-        self.connected = False
-        self.__lock = Lock()
-        self.__pkt_buffer = None
-        
-    def connect(self):
-        if self.connected == True:
-            return False
-
-        if self.debug: self.ws.enableTrace()
-        self.ws = websocket.WebSocketApp(f"ws://{self.server}/ws",
-            on_open=self.__on_open,
-            on_message=self.__on_message,
-            on_error=self.__on_error,
-            on_close=self.__on_close)
-        
-        # Start thread
-        self.thread = Thread(target=self.ws.run_forever, daemon=True)
-        self.thread.start()
-
-    def is_connected(self) -> bool:
-        return self.connected
-
-    def send(self, pkt:str):
-        self.ws.send(pkt)
-    
-    def receive(self) -> str:
-        if not self.__pkt_buffer:
-            return None
-        with self.__lock:
-            pkt = self.__pkt_buffer
-            self.__pkt_buffer = None
-        return pkt
-    
-
-    # WS methods
-    def __on_message(self, ws, pkt:str):
-        with self.__lock:
-            self.__pkt_buffer = pkt
-
-    def __on_error(self, ws, err):
+    while True:
         try:
-            raise err
-        except KeyboardInterrupt:
-            self.ws.close()
-
-    def __on_open(self, ws):
-        with self.__lock:
-            self.connected = True
-
-    def __on_close(self, ws, code, msg):
-        with self.__lock:
-            self.connected = False
-        exit(0)
-
-
+            # Get input
+            cmd = input("\n> ")
+        except (EOFError, KeyboardInterrupt):
+            malw.disconnect()
+            break
+        
+        # Exit case
+        if cmd == "exit":
+            malw.disconnect()
+            break
+        
+        # Send and listen
+        resp = malw.execute(cmd)
+        print(resp)
+    
+    # Exit
+    
 
 
 
@@ -108,18 +48,89 @@ def wait_for(func, expected, delay, timeout):
             return None
     return rtn
 
-def execute_cmd(ws, cmd, timeout=3, delay=0.1):
-    ws.send(cmd)
-    rtn = None
-    while type(rtn := ws.receive()) is not str:
-        time.sleep(delay)
-        if (timeout := timeout - delay) < 0:
+
+class WebSocketClient:
+    def __init__(self, server:str, debug:bool):
+        self.server = server
+        self.debug = debug
+        self.connected = False
+
+        self.__ws = None
+        self.__lock = Lock()
+        self.__pkt_buffer = None
+        
+    def connect(self):
+        if self.connected == True:
+            return False
+
+        if self.debug: self.__ws.enableTrace()
+        self.__ws = websocket.WebSocketApp(f"ws://{self.server}/ws",
+            on_open=self.__on_open,
+            on_message=self.__on_message,
+            on_error=self.__on_error,
+            on_close=self.__on_close)
+
+        # Start thread
+        self.thread = Thread(target=self.__ws.run_forever, daemon=True)
+        self.thread.start()
+    
+    def disconnect(self):
+        self.__ws.close()
+
+    def is_connected(self) -> bool:
+        return self.connected
+    
+    def send(self, pkt:str):
+        self.__ws.send(pkt)
+    
+    def listen(self) -> str:
+        if not self.__pkt_buffer:
             return None
-    return rtn
+        with self.__lock:
+            pkt = self.__pkt_buffer
+            self.__pkt_buffer = None
+        return pkt
+    
+    def execute(self, cmd:str, delay=0.1, timeout=5) -> str:
+        self.send(cmd+"\n")
+        while time.sleep(delay) == None: # (identical to "while True" but calls sleep)
+            if type(resp := self.listen()) is str:
+                return resp
+            elif (timeout := timeout - delay) < 0:
+                raise websocket.WebSocketTimeoutException("Command timed out")
     
 
+    # WS methods
+    def __on_message(self, ws, pkt:str):
+        with self.__lock:
+            self.__pkt_buffer = pkt
+
+    def __on_error(self, ws, err):
+        try:
+            raise err
+        except KeyboardInterrupt:
+            self.__ws.close()
+
+    def __on_open(self, ws):
+        with self.__lock:
+            self.connected = True
+
+    def __on_close(self, ws, code, msg):
+        with self.__lock:
+            self.connected = False
+        print("\n"+" Disconnected ".center(100, "="))
+        exit(0)
+
+
+
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--trace", help="Enable WS tracing", action="store_true")
-    main(parser.parse_args())
+    parser.add_argument("-d", "--debug", help="Enable WS tracing", action="store_true")
+    args = parser.parse_args()
+
+    try:   main(args)
+    except websocket.WebSocketException as e:
+        if args.debug == False: print(e, end="\n\n")
+        else: raise e
